c0eeb49dfcea1dc8392b667c7fac4aefcf440dd9
rinaldi <darksideofbotix@gmail.com>
2020-02-28 22:31:47 +0700

    Design tanpa vue

---
+++ src/components/Calculator.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="calculator">
+    <div class="display">123156156</div>
+    <div class="btn">C</div>
+    <div class="btn">+/-</div>
+    <div class="btn">%</div>
+    <div class="btn operator">/</div>
+    <div class="btn">7</div>
+    <div class="btn">8</div>
+    <div class="btn">9</div>
+    <div class="btn operator">x</div>
+    <div class="btn">4</div>
+    <div class="btn">5</div>
+    <div class="btn">6</div>
+    <div class="btn operator">-</div>
+    <div class="btn">1</div>
+    <div class="btn">2</div>
+    <div class="btn">3</div>
+    <div class="btn operator">+</div>
+    <div class="btn ziro">0</div>
+    <div class="btn">.</div>
+    <div class="btn operator">=</div>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+.calculator {
+  margin: 0 auto;
+  width: 400px;
+  display: grid;
+  font-size: 40px;
+  grid-template-columns: repeat(4, 1fr);
+  grid-auto-rows: minmax(50px, auto);
+  text-align: center;
+}
+
+.display {
+  grid-column: 1/5;
+  background-color:#333;
+  color: white;
+}
+
+.ziro {
+  grid-column: 1/3;
+}
+
+.btn {
+  background-color: #F2F2F2;
+  border: 1px solid #999;
+}
+
+.operator {
+  background-color: orange;
+  color: white;
+}
+</style>
